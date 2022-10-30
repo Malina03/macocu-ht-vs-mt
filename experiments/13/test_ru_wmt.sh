f68014099ea67a241121be42cf8a381ad344ee72
Malina03 <malina.chichirau@gmail>
2022-10-30 18:23:09 +0100

    removed old .out files

---
+++ experiments/13/test_ru_wmt.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+#SBATCH --job-name='13_test_ru'
+#SBATCH --partition=gpushort
+#SBATCH --time=01:00:00
+#SBATCH --gres=gpu:v100:1
+#SBATCH --ntasks 1
+#SBATCH --mem=16GB
+#SBATCH --output=/dev/null
+#SBATCH --mail-type=FAIL
+#SBATCH --mail-user=m.chichirau@student.rug.nl
+#SBATCH --array=1-4
+
+
+export TRANSFORMERS_CACHE=/data/pg-macocu/MT_vs_HT/cache/huggingface
+export WANDB_DISABLED=true  # for some reason this is necessary
+
+module purge
+module load Python/3.8.6-GCCcore-10.2.0
+source /data/$USER/.envs/macocu/bin/activate
+
+exp_id=13
+root_dir=/data/pg-macocu/MT_vs_HT/experiments/${exp_id}
+
+EXP_ID=13
+ROOT_DIR=/data/pg-macocu/MT_vs_HT/experiments/${EXP_ID}
+LANG = "ru"
+test_on = "wmt${SLURM_ARRAY_TASK_ID}"
+checkpoint = $ROOT_DIR/models/google/microsoft-deberta-v3-large_lr\=1e-05_bsz\=32_epochs\=5_seed\=1/checkpoint-1600
+arch="microsoft/deberta-v3-large"
+logfile="${root_dir}/results/${LANG}/${test_on}/eval_seed=${seed}.out"
+
+cd $HOME/HT-vs-MT/
+python classifier_trf.py \
+--root_dir $ROOT_DIR \
+--arch $arch \
+--test_on $test
+--test_on_language $LANG
+--load_model $checkpoint
+&> $logfile
+
