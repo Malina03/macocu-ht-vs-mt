da8c9cb117a0ce47b41ec703c117d825b3506553
Malina03 <malina.chichirau@gmail>
2022-10-31 14:37:47 +0100

    Removed whitespaces

--- experiments/13/test_ru.sh
+++ experiments/13/test_ru.sh
@@ -8,11 +8,9 @@
 #SBATCH --mem=16GB
 #SBATCH --output=/dev/null
 #SBATCH --array=1
-#SBATCH --mail-type=FAIL
+#SBATCH --mail-type=ALL
 #SBATCH --mail-user=m.chichirau@student.rug.nl
 
-export TRANSFORMERS_CACHE=/data/pg-macocu/MT_vs_HT/cache/huggingface
-export WANDB_DISABLED=true  # for some reason this is necessary
 
 module purge
 module load Python/3.8.6-GCCcore-10.2.0
@@ -20,11 +18,19 @@ source /data/$USER/.envs/macocu/bin/activate
 
 EXP_ID=13
 ROOT_DIR=/data/pg-macocu/MT_vs_HT/experiments/${EXP_ID}
-lang = "ru"
-test_set = "google"
-checkpoint = ${ROOT_DIR}/models/google/microsoft-deberta-v3-large_lr=1e-05_bsz=32_epochs=5_seed=1/checkpoint-1600
-arch = "microsoft/deberta-v3-large"
+
+lang="ru"
+test_set="google"
+checkpoint=${ROOT_DIR}/models/google/microsoft-deberta-v3-large_lr=1e-05_bsz=32_epochs=5_seed=1/checkpoint-1600
+arch="microsoft/deberta-v3-large"
 logfile="${ROOT_DIR}/results/ru/google/eval_seed=${SLURM_ARRAY_TASK_ID}.out"
+
+if [ $mt == "google" ]; then
+    flags="--use_google_data"
+else
+    flags=""
+fi
+
 
 cd $HOME/HT-vs-MT/
 python classifier_trf_hf.py \
@@ -32,5 +38,6 @@ python classifier_trf_hf.py \
 --arch $arch \
 --test $test_set \
 --test_on_language $lang \
---load_model $checkpoint 
+--load_model $checkpoint \
+$flags \
 &> $logfile
--- experiments/13/test_ru_wmt.sh
+++ experiments/13/test_ru_wmt.sh
@@ -19,14 +19,12 @@ module purge
 module load Python/3.8.6-GCCcore-10.2.0
 source /data/$USER/.envs/macocu/bin/activate
 
-exp_id=13
-root_dir=/data/pg-macocu/MT_vs_HT/experiments/${exp_id}
 
 EXP_ID=13
 ROOT_DIR=/data/pg-macocu/MT_vs_HT/experiments/${EXP_ID}
-lang = "ru"
-test_set= "wmt${SLURM_ARRAY_TASK_ID}"
-checkpoint = ${ROOT_DIR}/models/google/microsoft-deberta-v3-large_lr=1e-05_bsz=32_epochs=5_seed=1/checkpoint-1600
+lang="ru"
+test_set="wmt${SLURM_ARRAY_TASK_ID}"
+checkpoint=${ROOT_DIR}/models/google/microsoft-deberta-v3-large_lr=1e-05_bsz=32_epochs=5_seed=1/checkpoint-1600
 arch="microsoft/deberta-v3-large"
 logfile="${ROOT_DIR}/results/ru/wmt/eval_seed=${SLURM_ARRAY_TASK_ID}.out"
 
