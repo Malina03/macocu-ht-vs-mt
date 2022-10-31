1732b4cc6fb79f060533a4a7a313bf4153e8e306
Malina03 <malina.chichirau@gmail>
2022-10-31 18:45:21 +0100

    Fixed folder path for wmt

--- experiments/13/test_ru_wmt.sh
+++ experiments/13/test_ru_wmt.sh
@@ -12,9 +12,6 @@
 #SBATCH --array=1-4
 
 
-export TRANSFORMERS_CACHE=/data/pg-macocu/MT_vs_HT/cache/huggingface
-export WANDB_DISABLED=true  # for some reason this is necessary
-
 module purge
 module load Python/3.8.6-GCCcore-10.2.0
 source /data/$USER/.envs/macocu/bin/activate
@@ -26,7 +23,13 @@ lang="ru"
 test_set="wmt${SLURM_ARRAY_TASK_ID}"
 checkpoint=${ROOT_DIR}/models/google/microsoft-deberta-v3-large_lr=1e-05_bsz=32_epochs=5_seed=1/checkpoint-1600
 arch="microsoft/deberta-v3-large"
-logfile="${ROOT_DIR}/results/ru/wmt/eval_seed=${SLURM_ARRAY_TASK_ID}.out"
+logfile="${ROOT_DIR}/results/ru/wmt${SLURM_ARRAY_TASK_ID}/eval_seed=${SLURM_ARRAY_TASK_ID}.out"
+
+if [ $mt == "google" ]; then
+    flags="--use_google_data"
+else
+    flags=""
+fi
 
 cd $HOME/HT-vs-MT/
 python classifier_trf_hf.py \
