377ddc5386c146a160e25d3d49a865f9fc81a64d
Malina03 <malina.chichirau@gmail>
2022-11-14 17:45:47 +0100

    changed agent count

--- experiments/7/sweep_tobias.sh
+++ experiments/7/sweep_tobias.sh
@@ -9,7 +9,6 @@
 #SBATCH --output=/dev/null
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=m.chichirau@student.rug.nl
-#SBATCH --array=1
 
 
 # export TRANSFORMERS_CACHE=/data/pg-macocu/MT_vs_HT/cache/huggingface
@@ -17,10 +16,10 @@
 
 EXP_ID=7
 root_dir=/data/pg-macocu/MT_vs_HT/experiments/${EXP_ID}
-seed=${SLURM_ARRAY_TASK_ID}
 
 module purge
 module load Python/3.8.6-GCCcore-10.2.0
 source /data/$USER/.envs/macocu/bin/activate
 
-wandb sweep --project ... sweep_tobias_params.yaml
+# wandb sweep --project HT-vs-MT-7 sweep_tobias_params.yaml
+wandb agent --count 1 malina03/HT-vs-MT-7/tguqnel3
