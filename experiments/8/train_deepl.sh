afb002c65f73ca84616e51ed890c48e71c162d63
Malina03 <malina.chichirau@gmail>
2022-12-12 13:11:06 +0200

    added training files for deberta exp 8

--- experiments/8/train_deepl.sh
+++ experiments/8/train_deepl.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#SBATCH --job-name='8_train'
+#SBATCH --job-name='8_d_train'
 #SBATCH --partition=gpu
 #SBATCH --time=02:00:00
 #SBATCH --gres=gpu:v100:1
@@ -18,14 +18,14 @@ root_dir=/data/pg-macocu/MT_vs_HT/experiments/${exp_id}
 
 module purge
 module load Python/3.8.6-GCCcore-10.2.0
-source $HOME/activate_py3.8.6
+source /data/$USER/.envs/macocu/bin/activate
 
 # Hyper-parameters
 arch="microsoft/deberta-v3-large"
 mt="deepl"
 learning_rate=1e-05
 bsz=32
-num_epochs=5
+num_epochs=10
 weight_decay=0
 max_grad_norm=1
 warmup_steps=200
@@ -39,24 +39,25 @@ else
     flags=""
 fi
 
-log_model_name=$(echo $arch | sed 's/\//-/g')
+log_model_name="deberta"
 # Make sure the logdir specified below corresponds to the directory defined in the
 # main() function of the `classifier_trf_hf.py` script!
-logdir="${root_dir}/models/${mt}/${log_model_name}_lr=${learning_rate}_bsz=${bsz}_seed=${seed}/"
-logfile="${logdir}/train.out"
+logdir="/data/pg-macocu/MT_vs_HT/experiments/14/models/${mt}/${log_model_name}/"
+logfile="/data/pg-macocu/MT_vs_HT/experiments/14/results/${mt}/dev/${mt}/train.out"
 mkdir -p $logdir
 
+
 # Copy source code
-mkdir -p $logdir/src
-cp $HOME/MaCoCu/student_project_mt_ht/classifier_trf_hf.py $logdir/src
+# mkdir -p $logdir/src
+# cp $HOME/MaCoCu/student_project_mt_ht/classifier_trf_hf.py $logdir/src
 
-# Copy this script
-cp $(realpath $0) $logdir
+# # Copy this script
+# cp $(realpath $0) $logdir
 
 
-cd $HOME/MaCoCu/student_project_mt_ht/
-python classifier_trf_hf.py \
+python $HOME/HT-vs-MT/classifier_trf_hf.py \
 --root_dir $root_dir \
+--output_dir $logdir \
 --arch $arch \
 --learning_rate $learning_rate \
 --batch_size $bsz \
@@ -67,5 +68,6 @@ python classifier_trf_hf.py \
 --label_smoothing $label_smoothing \
 --dropout $dropout \
 --seed $seed \
+--strategy "epoch" \
 $flags \
 &> $logfile
