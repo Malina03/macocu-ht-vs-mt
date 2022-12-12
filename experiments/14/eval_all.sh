b8d2b39738e737b8ee479ae419dfa005c268ca97
Malina03 <malina.chichirau@gmail>
2022-12-12 22:34:31 +0200

    syntax

--- experiments/14/eval_all.sh
+++ experiments/14/eval_all.sh
@@ -37,13 +37,13 @@ for trained_on in ${models[@]}; do
         for eval_on in ${eval_sets[@]}; do
 
             if [ $trained_on == "google" && $arch_folder == "mdeberta"]; then
-                ckpt=2580
+                ckpt="2580"
             elif [ $trained_on == "deepl" && $arch_folder == "mdeberta"]; then
-                ckpt=5155
+                ckpt="5155"
             elif [ $trained_on == "google" && $arch_folder == "deberta"]; then
-                ckpt=1032
+                ckpt="1032"
             elif [ $trained_on == "deepl" && $arch_folder == "deberta"]; then
-                ckpt=1548
+                ckpt="1548"
             fi
             
             checkpoint="${ROOT_DIR}/models/${trained_on}/${arch_folder}/checkpoint-${ckpt}"
