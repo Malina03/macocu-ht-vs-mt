53ccb832c44ca845c46ee656fb61ecfab4b02fb8
Malina03 <malina.chichirau@gmail>
2022-12-13 15:56:07 +0200

    rm temp

--- mt_eval/comet.sh
+++ mt_eval/comet.sh
@@ -18,23 +18,23 @@ sets=("deepl" "google" "wmt1" "wmt2" "wmt3" "wmt4")
 
 for lang in ${languages[@]}; do
     for test_set in ${sets[@]}; do
-        out=$TMPDIR/out.txt
-        ref=$TMPDIR/ref.txt
-        src=$TMPDIR/src.txt
+        # out=$TMPDIR/out.txt
+        # ref=$TMPDIR/ref.txt
+        # src=$TMPDIR/src.txt
         dir=/data/pg-macocu/MT_vs_HT/experiments/comet/data/${lang}-en/${test_set}/
         if [[ $test_set = "deepl" ]]; then
-            cat $dir/*.deepl.en >> $out
+            out=$dir/*.deepl.en
         elif [[ $test_set = "google" ]]; then
-            cat $dir/*.en.google >> $out
+            out=$dir/*.en.google
         elif [[ $test_set =  wmt{1..4} ]]; then
             cat $dir/*.wmt >> $out
         else
             echo "$test_set and $lang combination is not a valid."
             exit 1
         fi
-        cat $dir/trans_*.txt >> $ref
-        cat $dir/org_*.txt >> $src
+        ref=$dir/trans_*.txt
+        src=$dir/org_*.txt
         comet-score -s $src -t $out -r $ref > /data/pg-macocu/MT_vs_HT/experiments/comet/results/${test_set}.${lang}.comet
-        rm -r $TMPDIR/*
+        # rm -r $TMPDIR/*
     done
 done
