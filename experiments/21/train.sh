7aa32e509a7024f63627653d2c2e81639de6f8ac
Malina03 <malina.chichirau@gmail>
2023-01-02 18:19:01 +0100

    added ckpt for 20 and seeds for 21

--- experiments/21/train.sh
+++ experiments/21/train.sh
@@ -7,7 +7,7 @@
 #SBATCH --ntasks 1
 #SBATCH --mem=16GB
 #SBATCH --output=/dev/null
-#SBATCH --array=1
+#SBATCH --array=1-3
 #SBATCH --mail-type=BEGIN,FAIL,END
 #SBATCH --mail-user=m.chichirau@student.rug.nl
 
