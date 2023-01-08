2bc705f99c587033e108bd4e6560d86c50fc392d
Malina03 <malina.chichirau@gmail>
2023-01-08 18:01:49 +0100

    fix var

--- data.py
+++ data.py
@@ -233,7 +233,7 @@ def load_corpus_multilingual_sentence_pairs(args, phase):
                 f"org_{lang}en_{lang}_wmt{wmt_year}.txt.en.google"
             ]:
                 # Translation from original text.
-                path_A = root_dir / f"data/{mt}/{phase}/{lang_apdx}-en/org_{lang}en_{lang}_wmt{wmt_year}.txt"
+                path_A = root_dir / f"data/{mt}/{phase}/{lang}-en/org_{lang}en_{lang}_wmt{wmt_year}.txt"
             else:  # fail
                 raise RuntimeError(
                     f"Unrecognized file name: {path_B.name}. Take a look "
