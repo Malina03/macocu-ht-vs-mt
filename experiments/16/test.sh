78204f175e6ede568f2b297c776d86e3516792ad
Malina03 <malina.chichirau@gmail>
2023-01-17 11:42:56 +0100

    updated testing mdeberta

--- experiments/16/test.sh
+++ experiments/16/test.sh
@@ -20,15 +20,15 @@ source /data/$USER/.envs/macocu/bin/activate
 EXP_ID=16
 ROOT_DIR=/data/pg-macocu/MT_vs_HT/experiments/${EXP_ID}
 
-# arch="microsoft/mdeberta-v3-base"
-# arch_folder="mdeberta"
+arch="microsoft/mdeberta-v3-base"
+arch_folder="mdeberta"
 
-arch="microsoft/deberta-v3-large"
-arch_folder="deberta"
+# arch="microsoft/deberta-v3-large"
+# arch_folder="deberta"
 learning_rate=1e-05
 bsz=32
-# trained_on="google"
-trained_on="deepl"
+trained_on="google"
+# trained_on="deepl"
 test_set="test"
 eval_sets=("zh" "de" "ru")
 seeds=(1 2 3)
@@ -45,10 +45,9 @@ for seed in ${seeds[@]}; do
     #     ckpt=315
     # fi
     checkpoint="${ROOT_DIR}/models/${trained_on}/${arch_folder}_${seed}/checkpoint-*"
-
     for eval_on in ${eval_sets[@]}; do
 
-        logdir="${ROOT_DIR}/results/${trained_on}/${test_set}/${eval_on}/"
+        logdir="${ROOT_DIR}/results/${trained_on}/${arch_folder}/${test_set}/${eval_on}/"
         logfile="${logdir}/eval_${seed}.out"
         mkdir -p $logdir
 
