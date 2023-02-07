035a72e3b533473590aff14ab0076dcf00ebc2fe
Malina03 <malina.chichirau@gmail>
2023-02-07 12:40:27 +0100

    added majority testing

--- experiments/29/test_majority.sh
+++ experiments/29/test_majority.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-#SBATCH --job-name='15_eval'
-#SBATCH --partition=gpushort
-#SBATCH --time=01:30:00
+#SBATCH --job-name='29_eval'
+#SBATCH --partition=gpu
+#SBATCH --time=02:00:00
 #SBATCH --gres=gpu:v100:1
 #SBATCH --ntasks 1
 #SBATCH --mem=16GB
@@ -16,42 +16,36 @@ module purge
 module load Python/3.8.6-GCCcore-10.2.0
 source /data/$USER/.envs/macocu/bin/activate
 
-
-EXP_ID=15
+EXP_ID=29
 ROOT_DIR=/data/pg-macocu/MT_vs_HT/experiments/${EXP_ID}
 
-# arch="microsoft/mdeberta-v3-base"
-# arch_folder="mdeberta"
 
-arch="microsoft/deberta-v3-large"
-arch_folder="deberta"
-learning_rate=1e-05
-bsz=32
+# Hyper-parameters
+arch="microsoft/mdeberta-v3-base"
+arch_folder="mdeberta"
 trained_on="google"
 # trained_on="deepl"
-test_set="test"
 eval_sets=("zh" "de" "ru")
 seeds=(1 2 3 4 5 6 7 8 9 10)
 
 cd $HOME/HT-vs-MT/
-
 for seed in ${seeds[@]}; do
-    # if [ $seed == "1" ]; then 
+    # if [ $seed == 1 ]; then 
+    #     ckpt=1032
+    # fi
+    # if [ $seed == 2 ]; then
     #     ckpt=2064
     # fi
-    # if [ $seed == "2" ]; then
-    #     ckpt=1548
+    # if [ $seed == 3 ]; then
+    #     ckpt=4128
     # fi
-    # if [ $seed == "3" ]; then
-    #     ckpt=1548 ## it's the same as for seed 2, checked this
-    # fi
-    
-    checkpoint="${ROOT_DIR}/models/${trained_on}/${arch_folder}_${seed}/checkpoint-*"
+    checkpoint="/data/pg-macocu/MT_vs_HT/experiments/22/models/${trained_on}/${arch_folder}_${seed}/checkpoint-*"
+
     for eval_on in ${eval_sets[@]}; do
 
-        # logdir="${ROOT_DIR}/results/${trained_on}/${arch_folder}/${test_set}/${eval_on}/"
+        # logdir="${ROOT_DIR}/results/${trained_on}/test/${eval_on}/"
         logdir="/data/$USER/MT_vs_HT/experiments/${EXP_ID}/results/${trained_on}/${test_set}/${eval_on}/"
-        logfile="${logdir}/eval_${seed}.out"
+        logfile="${logdir}/eval_majority_${seed}.out"
         mkdir -p $logdir
 
         if [ $trained_on == "google" ]; then
@@ -59,22 +53,17 @@ for seed in ${seeds[@]}; do
         else
             flags=""
         fi
-
-        if [ $test_set == "dev" ]; then
-            test_flags="--eval"
-        else
-            test_flags="--test $trained_on"
-        fi
         
         python classifier_trf_hf.py \
         --root_dir $ROOT_DIR \
-        --batch_size 16 \
+        --batch_size 8 \
         --arch $arch \
-        --test_on_language ${eval_on} \
         --load_model $checkpoint \
+        --load_sentence_pairs "multilingual" \
+        --use_majority_classification \
+        --test $eval_on \
+        --max_length 512 \
         $flags \
-        $test_flags \
         &> $logfile
     done
 done
-
