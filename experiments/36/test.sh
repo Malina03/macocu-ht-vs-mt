c87c42de266754ecd8aa67958247d660d78bc536
Malina03 <malina.chichirau@gmail>
2023-02-08 15:39:28 +0100

    changed to gpu

--- experiments/36/test.sh
+++ experiments/36/test.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 #SBATCH --job-name='36_eval'
-#SBATCH --partition=regular
-#SBATCH --time=10:00:00
+#SBATCH --partition=gpu
+#SBATCH --time=03:00:00
 #SBATCH --ntasks 1
 #SBATCH --mem=16GB
 #SBATCH --output=/dev/null
