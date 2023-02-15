49bc999edb6ca08f5731de67c0a733e58a8073b4
Malina03 <malina.chichirau@gmail>
2023-02-15 17:17:04 +0100

    moved to own data

--- experiments/35/train_ft.sh
+++ experiments/35/train_ft.sh
@@ -16,8 +16,8 @@
 # export WANDB_DISABLED=true  # for some reason this is necessary
 
 exp_id=35
-root_dir=/data/pg-macocu/MT_vs_HT/experiments/${exp_id}
-# root_dir=/data/$USER/MT_vs_HT/experiments/${exp_id}
+# root_dir=/data/pg-macocu/MT_vs_HT/experiments/${exp_id}
+root_dir=/data/$USER/MT_vs_HT/experiments/${exp_id}
 
 module purge
 module load Python/3.8.6-GCCcore-10.2.0
@@ -47,7 +47,7 @@ fi
 
 log_model_name="mdeberta_ft"
 arch_folder="mdeberta"
-checkpoint="/data/pg-macocu/MT_vs_HT/experiments/22/models/${mt}/${arch_folder}_${seed}/checkpoint-*"
+checkpoint="/data/$USER/MT_vs_HT/experiments/22/models/${mt}/${arch_folder}_${seed}/checkpoint-*"
 
 logdir="${root_dir}/models/${mt}/${log_model_name}_${seed}/"
 outputdir="${root_dir}/results/${mt}/dev"
