c856a9284f9784dad0fe831cc7182ac01f1e27be
Malina03 <malina.chichirau@gmail>
2023-03-21 18:11:58 +0100

    included mt as arg, adde opus extension

---
+++ experiments/22/train_deepl.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+
+#SBATCH --job-name='22_d_train'
+#SBATCH --partition=gpu
+#SBATCH --time=04:00:00
+#SBATCH --gres=gpu:v100:1
+#SBATCH --ntasks 1
+#SBATCH --mem=16GB
+#SBATCH --output=/dev/null
+#SBATCH --array=1-3
+#SBATCH --mail-type=BEGIN,FAIL,END
+#SBATCH --mail-user=m.chichirau@student.rug.nl
+
+
+export TRANSFORMERS_CACHE=/data/pg-macocu/MT_vs_HT/cache/huggingface
+export WANDB_DISABLED=true  # for some reason this is necessary
+
+exp_id=22
+root_dir=/data/pg-macocu/MT_vs_HT/experiments/${exp_id}
+
+module purge
+module load Python/3.8.6-GCCcore-10.2.0
+source /data/$USER/.envs/macocu/bin/activate
+
+# Hyper-parameters
+arch="microsoft/mdeberta-v3-base"
+# mt="google"
+mt="deepl"
+# learning_rate=5e-05
+# bsz=32
+# max_length=3072
+learning_rate=1e-05
+bsz=16
+max_length=512
+num_epochs=10
+weight_decay=0
+max_grad_norm=1
+warmup_steps=400
+label_smoothing=0.0
+dropout=0.1
+
+seed=${SLURM_ARRAY_TASK_ID}
+
+# if [ $mt == "google" ]; then
+#     flags="--use_google_data"
+# else
+#     flags=""
+# fi
+
+log_model_name="mdeberta"
+logdir="${root_dir}/models/${mt}/${log_model_name}_${seed}/"
+outputdir="${root_dir}/results/${mt}/dev"
+logfile="${outputdir}/train_${seed}.out"
+mkdir -p $outputdir
+mkdir -p $logdir
+
+
+cd $HOME/HT-vs-MT/
+python classifier_trf_hf.py \
+--root_dir $root_dir \
+--output_dir $logdir \
+--arch $arch \
+--mt $mt \
+--learning_rate $learning_rate \
+--batch_size $bsz \
+--num_epochs $num_epochs \
+--weight_decay $weight_decay \
+--max_grad_norm $max_grad_norm \
+--warmup_steps $warmup_steps \
+--label_smoothing $label_smoothing \
+--dropout $dropout \
+--seed $seed \
+--strategy "epoch" \
+--load_sentence_pairs "multilingual" \
+--max_length $max_length \
+&> $logfile
