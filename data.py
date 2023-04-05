b171c98631005a2fe58754177fbadb66c8d0ec20
Malina03 <malina.chichirau@gmail>
2023-04-05 14:49:14 +0200

    added train 40

--- data.py
+++ data.py
@@ -221,6 +221,7 @@ def load_corpus(args, phase, split_docs_by_sentence=False):
             list((root_dir / f"data/{mt}/{phase}/{apdx}").glob("*.deepl.en"))
             + list((root_dir / f"data/{mt}/{phase}/{apdx}").glob("*.en.google"))
             + list((root_dir / f"data/{mt}/{phase}/{apdx}").glob("*.opus.en"))
+            + list((root_dir / f"data/{mt}/{phase}/{apdx}").glob("*.wmt"))
             + list((root_dir / f"data/{mt}/{phase}/{apdx}").glob("*.wmt1"))
             + list((root_dir / f"data/{mt}/{phase}/{apdx}").glob("*.wmt2"))
             + list((root_dir / f"data/{mt}/{phase}/{apdx}").glob("*.wmt3"))
@@ -281,7 +282,7 @@ def load_corpus_multilingual_sentence_pairs(args, phase, split_docs_by_sentence=
         
     print("=> Loading {} corpus...".format(phase))
 
-    _mt_suffixes = [".txt.en.google", '.deepl.en', ".opus.en", 'wmt1', 'wmt2', 'wmt3','wmt4']
+    _mt_suffixes = [".txt.en.google", '.deepl.en', ".opus.en", 'wmt', 'wmt1', 'wmt2', 'wmt3','wmt4']
 
     corpus_data = []
     root_dir = Path(args.root_dir).resolve()
@@ -333,7 +334,8 @@ def load_corpus_multilingual_sentence_pairs(args, phase, split_docs_by_sentence=
                 f"org_{lang}en_{lang}_wmt{wmt_year}.wmt1",
                 f"org_{lang}en_{lang}_wmt{wmt_year}.wmt2",
                 f"org_{lang}en_{lang}_wmt{wmt_year}.wmt3",
-                f"org_{lang}en_{lang}_wmt{wmt_year}.wmt4"
+                f"org_{lang}en_{lang}_wmt{wmt_year}.wmt4",
+                f"org_{lang}en_{lang}_wmt{wmt_year}.wmt"
             ]:
                 # Translation from original text.
                 if phase == "test":
