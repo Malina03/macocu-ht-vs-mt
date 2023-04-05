d1f7916f3c505f3eadca7a6c499f9b27f0865568
Malina03 <malina.chichirau@gmail>
2023-04-05 11:31:50 +0200

    added nodes

--- opus_mt/opusmt_code_translate/run_translation.sh
+++ opus_mt/opusmt_code_translate/run_translation.sh
@@ -2,6 +2,7 @@
 
 #SBATCH --job-name='opus_mt'
 #SBATCH --partition=gpu
+#SBATCH --nodes=1
 #SBATCH --time=01:30:00
 #SBATCH --gpus-per-node=a100:1
 #SBATCH --mem=16GB
@@ -18,4 +19,4 @@ logfile="/scratch/s3412768/opus_mt/translation.out"
 
 cd /home1/s3412768/HT-vs-MT/opus_mt/opusmt_code_translate
 
-python -u translate.py > $logfile
+python translate.py > $logfile
