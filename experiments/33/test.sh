08f2d360d119e790654109d82c7b57c9713cf795
Malina03 <malina.chichirau@gmail>
2023-04-27 20:08:19 +0200

    fixed test script

--- experiments/33/test.sh
+++ experiments/33/test.sh
@@ -37,16 +37,6 @@ seeds=(1 2 3)
 cd $HOME/HT-vs-MT/
 
 for seed in ${seeds[@]}; do
-    # if [ $seed == "1" ]; then 
-    #     ckpt=2064
-    # fi
-    # if [ $seed == "2" ]; then
-    #     ckpt=1548
-    # fi
-    # if [ $seed == "3" ]; then
-    #     ckpt=1548 ## it's the same as for seed 2, checked this
-    # fi
-    
     checkpoint="${ROOT_DIR}/models/${trained_on}/${arch_folder}_${seed}/checkpoint-*"
     for eval_on in ${eval_sets[@]}; do
 
